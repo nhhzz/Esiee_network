d77fed2e0d96f7bd3fb4e6bb7951e6830171f51c
oelaabed <omar.el-aabed@edu.esiee.fr>
2026-03-08 13:43:42 +0100

    Ajout : mot de passe oublié

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from . import views
 
 app_name = 'users'
@@ -6,6 +7,38 @@ app_name = 'users'
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.user_login, name='login'),
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='users/password_reset_form.html',
+            email_template_name='users/password_reset_email.txt',
+            subject_template_name='users/password_reset_subject.txt',
+            success_url=reverse_lazy('users:password_reset_done'),
+        ),
+        name='password_reset',
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html',
+        ),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html',
+            success_url=reverse_lazy('users:password_reset_complete'),
+        ),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html',
+        ),
+        name='password_reset_complete',
+    ),
     path('signup/', views.signup, name='signup'),
     path('verify-email/<uidb64>/<token>/', views.verify_email, name='verify_email'),
     path('logout/', views.user_logout, name='logout'),
